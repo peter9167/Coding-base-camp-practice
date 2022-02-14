36cee46995353516cf76551081a8b74d2462aec6
Peter <peter9167@naver.com>
2022-02-14 21:38:49 +0900

    commit

--- test.js
+++ test.js
@@ -1,16 +1,4 @@
-function menuGlobal() {
-    console.log("오늘 저녁은 " + this.name);
-}
-
-var myDiner = {
-    name: "김치찌개",
-    menu: menuGlobal
-}
-myDiner.menu();
-
-
-var yourDiner = {
-    name: "된장찌개",
-    menu: menuGlobal
-}
-yourDiner.menu();
+function myFunction() {
+    return this;
+  }
+  console.log(myFunction()); //Window
