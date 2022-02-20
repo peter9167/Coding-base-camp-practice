4163aecd368836b6fd8cdd9eecbd5bb243e0b6b7
Peter <peter9167@naver.com>
2022-02-20 21:04:57 +0900

    commit

--- PyQt/Chapter4/007_위젯_진행바(QProgressBar).py
+++ PyQt/Chapter4/007_위젯_진행바(QProgressBar).py
@@ -10,8 +10,8 @@ class 진행바(QWidget):
         super().__init__()
         self.UI초기화()
 
-    def UI초기화(self):
-        self.bar1 = QProgressBar(self) 
+    def UI초기화(self): 
+        self.bar1 = QProgressBar(self) # QProgressBar 생성
         self.bar1.setOrientation(Qt.Vertical) 
         self.bar1.setGeometry(50, 20, 50, 300)
 
@@ -22,6 +22,11 @@ class 진행바(QWidget):
         self.label1 = QLabel('이 바의 범위는 ' + str(self.bar2.minimum())+" 부터 " +
                              str(self.bar2.maximum())+" 입니다.", self)
         self.label1.move(150,180)
+
+        # - setOrientation(Qt.Vertical) : 세로 진행바를 생성, 반대는 Horizontal(가로)
+        # - setRange(시작 숫자,끝 숫자) : 진행바의 범위를 설정
+        # - minimum() : 시작 숫자를 가져옵니다.
+        # - maximum() : 끝 숫자를 가져옵니다.
         
         self.label2 = QLabel('이곳에 첫 번째 바의 값이 나옵니다.',self)
         self.label2.move(130,300)
@@ -34,6 +39,10 @@ class 진행바(QWidget):
         self.value = 0 
         self.timer = QBasicTimer() 
         self.bar1.valueChanged.connect(self.changeValue)
+
+        # - 버튼이 클릭되면 runTimer 슬롯(이벤트) 호출
+        # - value 변수: 진행 상황 숫자
+        # - valueChanged : 값이 변할 때 changeValue() 호출
 
         self.setGeometry(300, 300, 400, 400)
         self.setWindowTitle('QProgressBar')
@@ -49,6 +58,9 @@ class 진행바(QWidget):
             
             self.btn.setText('중지')
 
+        # - timer가 활성화(isActive)되어 있으면 즉, 실행중이면 버튼을 클릭했을 때 stop()으로 중지
+        # - 그렇지 않다면 (실행중이 아니면) start(종료시간,이벤트가 수행되는 객체)을 시작
+
     def timerEvent(self, event): 
 
         if self.value >= self.bar1.maximum():  
@@ -59,7 +71,11 @@ class 진행바(QWidget):
         self.value +=1   
         self.bar1.setValue(self.value)  
 
-    def changeValue(self):
+        # - timerEvent는 이미 정의되어 있는 함수(슬롯)으로 파라미터에 추가로 e(event)를 사용
+        # - value 값이 설정된 최대값 이상이라면 타이머는 종료
+        # - 그렇지 않다면 value값을 1씩 더해 setValue를 통해 업데이트
+
+    def changeValue(self): # 진행바가 수행되는 도중 value 값을 라벨에 업데이트
         self.label2.setText(str(self.bar1.value()))
 
 
